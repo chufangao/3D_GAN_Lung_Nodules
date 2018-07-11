c93e888fdc5b2fbb478a10691d63afdde50c394d
andy1445 <gaoandy1445@gmail.com>
2018-07-11 17:25:34 -0500

    added files for prev year

--- showimages.py
+++ showimages.py
@@ -5,9 +5,9 @@ import numpy as np
 
 import pickle
 
-with open('images/generated_nodules100_.pickle','rb') as f:
+with open('generated_nodules50.pickle','rb') as f:
    posdat = pickle.load(f)
 
 for i in range(posdat.shape[3]):
-    print(np.max(posdat[i]))
-    plt.imsave('test'+str(i)+'.png',posdat[0,:,:,i,0])
+    gen_img = 0.5 * posdat[0,:,:,i,0] + 0.5
+    plt.imsave('test'+str(i)+'.png', gen_img, cmap='gray')
