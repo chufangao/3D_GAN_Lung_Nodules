491c4c0ff98afe935c0ba60827c02f1c1d502468
unfallible <stephenclark314@gmail.com>
2018-07-12 09:01:07 -0500

    Merge branch 'master' of https://github.com/andy1445/deep_learning_reu
    # Conflicts:
    #	our_models/3d_wgan_gp2.py

--- showimages.py
+++ showimages.py
@@ -5,9 +5,12 @@ import numpy as np
 
 import pickle
 
-with open('images/generated_nodules100_.pickle','rb') as f:
+EPOCH = '300'
+
+with open('C:\Users\sclark54\Desktop\generated_nodules'+EPOCH+'.pickle','rb') as f:
    posdat = pickle.load(f)
 
+dir = 'C:/Users/sclark54/Desktop/pics/epoch'+EPOCH+'/'
 for i in range(posdat.shape[3]):
     print(np.max(posdat[i]))
-    plt.imsave('test'+str(i)+'.png',posdat[0,:,:,i,0])
+    plt.imsave('nodule'+EPOCH+'_'+str(i)+'.png',posdat[0,:,:,i,0])
