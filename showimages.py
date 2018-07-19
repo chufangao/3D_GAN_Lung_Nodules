db7542d132edf1007082e84d6eb97a2e7147b186
andy1445 <gaoandy1445@gmail.com>
2018-07-19 16:58:11 -0500

    Merge branch 'master' of https://github.com/andy1445/deep_learning_reu
    # Conflicts:
    #	our_models/3d_wgan_gp2.py

--- showimages.py
+++ showimages.py
@@ -2,12 +2,28 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-
 import pickle
 
-with open('generated_nodules50.pickle','rb') as f:
-   posdat = pickle.load(f)
+with open('images/gen_nod0.pickle','rb') as f:
+    posdat = pickle.load(f)
+# with open('images/PositiveAugmented.pickle','rb') as f:
+#     posdat = pickle.load(f)
 
-for i in range(posdat.shape[3]):
-    gen_img = 0.5 * posdat[0,:,:,i,0] + 0.5
-    plt.imsave('test'+str(i)+'.png', gen_img, cmap='gray')
+threshold = .9
+# print(np.average(posdat, axis=(1,2,3,4))); exit()
+print('white imgs', [i for i, v in enumerate(posdat) if np.average(v) > threshold]); exit()
+posdat = np.array(posdat)
+print(posdat.shape)
+fig, axs = plt.subplots(3,6)
+
+cnt = 0
+for k in range(0,100):
+    for i in range(3):
+        for j in range(6):
+            axs[i,j].imshow(0.5 * posdat[k,:,:,cnt,0] + 0.5, cmap='gray')
+            # axs[i, j].imshow(0.5 * posdat[k, :, :, cnt] + 0.5, cmap='gray')
+            axs[i,j].axis('off')
+            cnt += 1
+
+    cnt = 0
+    fig.savefig('test'+str(k)+'.png')
