e923352ec4599d57cc231407f5856e9180774c5f
andy1445 <gaoandy1445@gmail.com>
2018-07-19 19:35:59 -0500

    updated viewing of nodules

--- showimages.py
+++ showimages.py
@@ -4,20 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-with open('images/gen_nod0.pickle','rb') as f:
-    posdat = pickle.load(f)
-# with open('images/PositiveAugmented.pickle','rb') as f:
-#     posdat = pickle.load(f)
+posdat = pickle.load(open('images/gen_nod0.pickle','rb'))
+posdat = pickle.load(open('images/PositiveAugmented.pickle','rb'))
 
 threshold = .9
 # print(np.average(posdat, axis=(1,2,3,4))); exit()
-print('white imgs', [i for i, v in enumerate(posdat) if np.average(v) > threshold]); exit()
+print('white imgs', [i for i, v in enumerate(posdat) if np.average(v) > threshold])
 posdat = np.array(posdat)
 print(posdat.shape)
 fig, axs = plt.subplots(3,6)
 
 cnt = 0
-for k in range(0,100):
+for k in range(0,16):
     for i in range(3):
         for j in range(6):
             axs[i,j].imshow(0.5 * posdat[k,:,:,cnt,0] + 0.5, cmap='gray')
