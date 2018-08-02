f7d65d7acfba3478e829faf278d0b61ec147be48
Chameleon Cloud User <cc@gpu.novalocal>
2018-08-02 21:39:48 +0000

    asdf

--- our_models/3DCNNaugmentedtraining2.py
+++ our_models/3DCNNaugmentedtraining2.py
@@ -49,20 +49,20 @@ experiment_name = sys.argv[1]
 
 EPOCHS = 20
 #the size of the noise vector
-latent_dim = 200
+latent_dim = 400
 
 #the number of fake positive examples and real negative examples to add to the base data set for each trial
-experiment_trials = [[0,0], [.1,0], [1.0,0], [2.0,0], [.1,.1], [1.0,1.0], [2.0,2.0]]
-experiment_trials.extend([[.1,0], [1.0,0], [2.0,0], [.1,.1], [1.0,1.0], [2.0,2.0]])
-experiment_trials.extend([[.3,0], [.4, 0], [.5,0], [.3,.3], [.4,.4], [.5,.5]])
-experiment_trials.extend([[.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3]])
-experiment_trials.extend([[.4,0], [.4,0], [.4,0], [.4,0], [.4,0], [.4,0], [.4,0], [.4,0], [.4,.4], [.4,.4], [.4,.4], [.4,.4]])
+experiment_trials = [[.3,0], [.4,0], [.5,0], [.3,.3], [.4,.4], [.5,.5], [1,1]]
+#experiment_trials.extend([[.1,0], [1.0,0], [2.0,0], [.1,.1], [1.0,1.0], [2.0,2.0]])
+#experiment_trials.extend([[.3,0], [.4, 0], [.5,0], [.3,.3], [.4,.4], [.5,.5]])
+#experiment_trials.extend([[.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3], [.3,.3]])
+#experiment_trials.extend([[.4,0], [.4,0], [.4,0], [.4,0], [.4,0], [.4,0], [.4,0], [.4,0], [.4,.4], [.4,.4], [.4,.4], [.4,.4]])
 
 #these examples are taken equally from both the positive and negative examples
 validation_percentage = .2
 
 #the file containing the model for generating new training data
-generator_file = 'saved_models/g1.h5'
+generator_file = 'saved_models/g2.h5'
 
 #the directory for saving models
 target_directory = 'saved_models/'
