557b8c4689e96e0b6cdbdd8a5bd94bb7c8aaba78
Chameleon Cloud User <cc@gpu.novalocal>
2018-08-15 01:58:36 +0000

    setup

---
+++ our_models/setup.sh
@@ -0,0 +1,9 @@
+mkdir saved_models
+sudo apt install python3-pip
+pip3 install setuptools
+pip3 install tensorflow-gpu
+pip3 install keras
+pip3 install sklearn
+pip3 install matplotlib
+pip3 install pandas
+
